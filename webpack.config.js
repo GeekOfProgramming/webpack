ab0ebbe887394c48a6519213255ca93b6d359834
HamidLotfalian <Lotfalian.work@gmail.com>
2022-08-06 19:24:58 +0200

    Lesson-3 adding 1

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const entryPoint = "src/script-1.js";
+const outputPoint = "dist";
+
+module.exports = {
+  //define entry point
+  entry: path.resolve(__dirname, entryPoint),
+
+  //define output
+  output: {
+    path: path.resolve(__dirname, outputPoint),
+    filename: "bundle.js",
+  },
+};
